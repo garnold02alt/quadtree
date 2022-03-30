406ac11117046c1fe17465eef34c9f9b6d7506e0
garnold02alt <garnold02alt@protonmail.com>
2022-03-30 12:41:04 +0200

    correct normals

--- src/gen.rs
+++ src/gen.rs
@@ -8,21 +8,21 @@ use rand::Rng;
 use crate::render::{Mesh, State, Vertex};
 
 const LATTICE_LEN: usize = 16;
-const P_COUNT: usize = LATTICE_LEN + 1;
+const P_LEN: usize = LATTICE_LEN + 1;
 const T_COUNT: usize = LATTICE_LEN * LATTICE_LEN * 2;
 
 pub struct Lattice {
-    points: [[Vector3<f64>; P_COUNT]; P_COUNT],
+    points: [[Vector3<f64>; P_LEN]; P_LEN],
     triangles: Vec<[u16; 3]>,
 }
 
 impl Lattice {
     pub fn new(face: Face) -> Self {
-        let scalar = 1.0 / (P_COUNT - 1) as f64;
-        let mut points = [[Vector3::zero(); P_COUNT]; P_COUNT];
+        let scalar = 1.0 / (P_LEN - 1) as f64;
+        let mut points = [[Vector3::zero(); P_LEN]; P_LEN];
 
-        for y in 0..P_COUNT {
-            for x in 0..P_COUNT {
+        for y in 0..P_LEN {
+            for x in 0..P_LEN {
                 let point = &mut points[y][x];
                 let flat = vec2(x as f64, y as f64).map(|e| (e * scalar - 0.5) * 2.0);
                 *point = face.orient(flat);
@@ -33,10 +33,10 @@ impl Lattice {
 
         for y in 0..LATTICE_LEN {
             for x in 0..LATTICE_LEN {
-                let i0 = (y * P_COUNT + x) as u16;
-                let i1 = (y * P_COUNT + x + 1) as u16;
-                let i2 = ((y + 1) * P_COUNT + x + 1) as u16;
-                let i3 = ((y + 1) * P_COUNT + x) as u16;
+                let i0 = (y * P_LEN + x) as u16;
+                let i1 = (y * P_LEN + x + 1) as u16;
+                let i2 = ((y + 1) * P_LEN + x + 1) as u16;
+                let i3 = ((y + 1) * P_LEN + x) as u16;
 
                 triangles.push([i0, i1, i2]);
                 triangles.push([i0, i2, i3]);
@@ -51,7 +51,7 @@ impl Lattice {
         let color: [f32; 3] = rand::thread_rng().gen();
         let color: Vector3<f32> = color.into();
 
-        let vertices = self
+        let mut vertices = self
             .points
             .into_iter()
             .flatten()
@@ -60,11 +60,31 @@ impl Lattice {
 
                 Vertex {
                     position: mkpoint(spherical, &noise).cast().unwrap(),
-                    normal: spherical.cast().unwrap(),
+                    normal: Vector3::zero(),
                     color,
                 }
             })
             .collect::<Vec<_>>();
+
+        for y in 0..LATTICE_LEN {
+            for x in 0..LATTICE_LEN {
+                let p0 = vertices[y * P_LEN + x].position;
+                let p1 = vertices[y * P_LEN + x + 1].position;
+                let p2 = vertices[(y + 1) * P_LEN + x].position;
+                let e0 = p1 - p0;
+                let e1 = p2 - p0;
+                let normal = e0.cross(e1).normalize();
+
+                vertices[y * P_LEN + x].normal += normal;
+                vertices[y * P_LEN + x + 1].normal += normal;
+                vertices[(y + 1) * P_LEN + x + 1].normal += normal;
+                vertices[(y + 1) * P_LEN + x].normal += normal;
+            }
+        }
+
+        for vertex in &mut vertices {
+            vertex.normal = vertex.normal.normalize();
+        }
 
         fn mkpoint(sph: Vector3<f64>, noise: &OpenSimplex) -> Vector3<f64> {
             let xyz: [f64; 3] = (sph * 10.0).into();
